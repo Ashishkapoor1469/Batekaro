d3e1f8fed1c71c034617060ec170fe78400db529
Ashish <kapoorashish713@gmail.com>
2025-04-13 22:19:44 +0530

    changes

--- app/Friends/page.tsx
+++ app/Friends/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import ConverstaionFallback from "@/components/shared/converstaion/ConverstaionFallback";
+import ConversationFallback from "@/components/shared/converstaion/ConverstaionFallback";
 import ItemList from "@/components/shared/item-list/ItemList";
 import React from "react";
 import AddFriendDialog from "./_components/AddFriendDialog";
@@ -7,36 +7,34 @@ import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { Loader2 } from "lucide-react";
 import Request from "./_components/Request";
-const page = () => {
+
+const Page = () => {
   const requests = useQuery(api.requests.get);
+
   return (
     <div className="py-4 w-full h-full flex gap-2 pe-2">
       <ItemList title="Friends" action={<AddFriendDialog />}>
-        {requests ? (
-          requests.length === 0 ? (
-            <p className="w-full h-full flex items-center">
-              No Friends request Found
-            </p>
-          ) : (
-            requests.map((request) => {
-              return (
-                <Request
-                  key={request.request._id}
-                  id={request.request._id}
-                  imageUrl={request.sender.imageUrl}
-                  username={request.sender.name}
-                  email={request.sender.email}
-                />
-              );
-            })
-          )
+        {requests === undefined ? (
+          <Loader2 className="h-8 w-8 animate-spin" />
+        ) : requests.length === 0 ? (
+          <p className="w-full h-full flex items-center">
+            No Friends request Found
+          </p>
         ) : (
-          <Loader2 className="h-8 w-8" />
+          requests.map((request) => (
+            <Request
+              key={request.request._id}
+              id={request.request._id}
+              imageUrl={request.sender.imageUrl}
+              username={request.sender.name}
+              email={request.sender.email}
+            />
+          ))
         )}
       </ItemList>
-      <ConverstaionFallback />
+      <ConversationFallback />
     </div>
   );
 };
 
-export default page;
+export default Page;
